9e2b1507f646818186a8aef8d46126ec1fc5ebb7
Roger Becerra <rogelio.becerra001@gmail.com>
2021-03-19 11:09:45 -0500

    latest

--- script.js
+++ script.js
@@ -10,69 +10,53 @@ let turn = 'X';
 
 function checkForWin() {
     if (moves[0] === moves[1] && moves[1] === moves[2]) {
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[0].classList.add('blink')
-            squares[1].classList.add('blink')
-            squares[2].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[0].classList.add('blink')
+        squares[1].classList.add('blink')
+        squares[2].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[3] === moves[4] && moves[4] === moves[5]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[3].classList.add('blink')
-            squares[4].classList.add('blink')
-            squares[5].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[3].classList.add('blink')
+        squares[4].classList.add('blink')
+        squares[5].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[6] === moves[7] && moves[7] === moves[8]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[6].classList.add('blink')
-            squares[7].classList.add('blink')
-            squares[8].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[6].classList.add('blink')
+        squares[7].classList.add('blink')
+        squares[8].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[0] === moves[3] && moves[3] === moves[6]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[0].classList.add('blink')
-            squares[3].classList.add('blink')
-            squares[6].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[0].classList.add('blink')
+        squares[3].classList.add('blink')
+        squares[6].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[1] === moves[4] && moves[4] === moves[7]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[1].classList.add('blink')
-            squares[4].classList.add('blink')
-            squares[7].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[1].classList.add('blink')
+        squares[4].classList.add('blink')
+        squares[7].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[2] === moves[5] && moves[5] === moves[8]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[2].classList.add('blink')
-            squares[5].classList.add('blink')
-            squares[8].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[2].classList.add('blink')
+        squares[5].classList.add('blink')
+        squares[8].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[0] === moves[4] && moves[4] === moves[8]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[0].classList.add('blink')
-            squares[4].classList.add('blink')
-            squares[8].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[0].classList.add('blink')
+        squares[4].classList.add('blink')
+        squares[8].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else if (moves[2] === moves[4] && moves[4] === moves[6]){
-        if (turnCount > 4) {
-            removeClickEventCallback()
-            squares[2].classList.add('blink')
-            squares[4].classList.add('blink')
-            squares[6].classList.add('blink')
-            winMessageDiv.innerHTML = `${turn} WON!!!`;
-        }
+        removeClickEventCallback()
+        squares[2].classList.add('blink')
+        squares[4].classList.add('blink')
+        squares[6].classList.add('blink')
+        winMessageDiv.innerHTML = `${turn} WON!!!`;
     } else {
         if (turnCount > 1) {
             console.log('no winner yet')
@@ -91,7 +75,7 @@ function handleClickEventCallback(event) {
     console.log(turnCount, turn, moves)
     moves[event.target.id] = turn
     event.target.innerText = turn
-    checkForWin()
+    if (turnCount > 4){checkForWin()}
     turnCount++
     turn = (turnCount % 2 === 0) ? "O" : "X";
     console.log(turnCount, turn, moves)
