5b4d1b090733f74a4262046ab8c0e96f578030c6
Roger Becerra <rogelio.becerra001@gmail.com>
2021-03-19 09:06:52 -0500

    Everything till now, lots of bugs

--- script.js
+++ script.js
@@ -1,1 +1,105 @@
-console.log('JavaScript link: Successful')
+console.log('JavaScript link: Successful');
+
+const moves = new Array(9);
+const squares = document.querySelectorAll('#board > div');
+const winMessageDiv = document.querySelector('.board-message');
+const board = document.querySelector('#board');
+const reset = document.querySelector('#restart-game')
+let turnCount = 0;
+let turn = 'X';
+
+function checkForWin() {
+    if (moves[0] === moves[1] && moves[1] === moves[2]) {
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[3] === moves[4] && moves[4] === moves[5]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[6] === moves[7] && moves[7] === moves[8]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[0] === moves[3] && moves[3] === moves[6]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[1] === moves[4] && moves[4] === moves[7]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[2] === moves[5] && moves[5] === moves[8]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[0] === moves[4] && moves[4] === moves[8]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else if (moves[2] === moves[4] && moves[4] === moves[6]){
+        if (turnCount > 1) {
+            removeClickEventCallback()
+            squares[0].classList.add('blink')
+            squares[1].classList.add('blink')
+            squares[2].classList.add('blink')
+            winMessageDiv.innerHTML = `${turn} WON!!!`;
+        }
+    } else {
+        if (turnCount > 1) {
+            console.log('no winner yet')
+        }
+    }
+}
+
+function removeClickEventCallback() {
+    squares.forEach(square => {
+        (square.innerText !== turn) ? square.innerText = '' : square.classList.add('blink')
+        square.removeEventListener('click', handleClickEventCallback)
+    })
+}
+
+function handleClickEventCallback(event) {
+    moves[event.target.id] = turn
+    checkForWin()
+    event.target.innerText = turn
+    turn = (turnCount % 2 === 0) ? "O" : "X";
+    turnCount++
+}
+
+squares.forEach((square, index) => {
+    square.setAttribute('id', `${index}`)
+    square.addEventListener('click', handleClickEventCallback, {once: true})
+})
+
+reset.addEventListener('click', () => {
+    location.reload();
+})
